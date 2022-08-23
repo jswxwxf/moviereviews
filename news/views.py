4696fa3593bd64a8bc367d4188716c82995351aa
Jeffrey Wang <jeffreywang@JeffreydeMacBook-Pro.local>
2022-08-23 14:59:48 +0800

    first version

---
+++ news/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from .models import News
+
+
+def news(request):
+    newss = News.objects.all()
+    return render(request, 'news.html', {
+        'newss': newss
+    })
